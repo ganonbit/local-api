cc9e287db4bcf5cf8f0e27982c04bf8ea84f64e1
jeremy perez <glyde06@hotmail.com>
2019-11-02 21:47:41 -0500

    Add the 'introspection: true' to new ApollowServer object

--- utils/apollo-server.js
+++ utils/apollo-server.js
@@ -33,6 +33,7 @@ export const createApolloServer = (schema, resolvers, models) => {
   return new ApolloServer({
     typeDefs: schema,
     resolvers,
+    introspection: true,
     context: async ({ req }) => {
       let authUser;
 
