2fbc588d58ec2c6da9da093da353623941259efd
Andrew Reese <ask@hyliancoder.com>
2019-11-04 17:13:58 -0600

    cleaned up posts unused testing. added better console logs to show what is going on when 'npm run seed' is ran

--- scripts/posts.js
+++ scripts/posts.js
@@ -2,21 +2,21 @@ import mongoose from 'mongoose';
 import faker from 'faker';
 import User from '../models/User';
 
-const usersIds = User
-  .find()
-  .populate('users') // <==
-  .exec(function(err, users){
-      console.log(err);
-      console.log(users);
-  });
+// const usersIds = User
+//   .find()
+//   .populate('users') // <==
+//   .exec(function(err, users){
+//       console.log(err);
+//       console.log(users);
+//   });
 
-  console.log(usersIds);
+//   console.log(usersIds);
 
   const postFactory = [
   {
     content: faker.lorem.words(5),
     image: faker.image.food(),
-    author: usersIds,
+    // author: usersIds,
     createdAt: faker.date.recent(),
     updatedAt: faker.date.recent(),
   }
--- scripts/seed.js
+++ scripts/seed.js
@@ -3,8 +3,8 @@ import { generateToken } from '../utils/generate-token';
 const AUTH_TOKEN_EXPIRY = '1y';
 
 const userFactory = User.fake();
-console.log("fake gen" + userFactory)
+console.log("generate fake user data: \n" + userFactory)
 
 const userSeed = new User(userFactory).save();
 generateToken(userSeed, process.env.SECRET, AUTH_TOKEN_EXPIRY),
-console.log("make fake user" + userSeed)
+console.log("create new user with fake data: \n" + userSeed)
