7c78d71aee0a21696e157b9c21496127b413aff5
Andrew Reese <ask@hyliancoder.com>
2019-11-04 18:21:39 -0600

    added some error tracking

--- scripts/seed.js
+++ scripts/seed.js
@@ -5,6 +5,13 @@ const AUTH_TOKEN_EXPIRY = '1y';
 const userFactory = User.fake();
 console.log("generate fake user data: \n" + userFactory)
 
-const userSeed = new User(userFactory).save();
-generateToken(userSeed, process.env.SECRET, AUTH_TOKEN_EXPIRY),
-console.log("create new user with fake data: \n" + userSeed)
+const userSeed = new User(userFactory);
+userSeed.markModified('user');
+userSeed.save(function(error,data){
+    if (error) {
+        console.log(error); 
+    }
+    console.log(data + " saved to the DB"); 
+    res.json({ token: generateToken(userSeed, process.env.SECRET, AUTH_TOKEN_EXPIRY), user: data });
+});
+
