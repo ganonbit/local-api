ac04b370361b0e8417ffb52b7a6f9d06d62e1cfa
jeremy perez <glyde06@hotmail.com>
2019-11-10 00:34:17 -0600

    Add script for syncing all users in Algolia

---
+++ generators/syncAlgoliaUsers.js
@@ -0,0 +1,22 @@
+import {} from 'dotenv/config';
+import User from '../models/User';
+import mongoose from 'mongoose';
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+});
+
+const syncUsersInAlgolia = async () => {
+  try {
+    await User.syncWithAlgolia();
+  } catch (error) {
+    console.log('The following Error ocurred:')
+    console.error(error);
+  }
+  mongoose.connection.close();
+}
+
+syncUsersInAlgolia();
