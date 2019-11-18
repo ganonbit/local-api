5256e6d72d2d06a6704d3c3953d41ca9d8f1f8ea
Andrew Reese <ask@hyliancoder.com>
2019-11-18 14:26:59 -0600

    updated index.js for models

---
+++ models/index.js
@@ -0,0 +1,25 @@
+import User from './User';
+import Message from './Message';
+import Post from './Post';
+import Like from './Like';
+import Follow from './Follow';
+import Comment from './Comment';
+import Notification from './Notification';
+import Reward from './Reward';
+import Event from './Event';
+import Achievement from './Achievement';
+import Point from './Point';
+
+export default {
+  User,
+  Message,
+  Post,
+  Like,
+  Follow,
+  Comment,
+  Notification,
+  Reward,
+  Event,
+  Achievement,
+  Point
+};
