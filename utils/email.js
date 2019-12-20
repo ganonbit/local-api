4d3fe48fdb961ff210a4122ae5d63ac31d093904
Andrew Reese <ask@hyliancoder.com>
2019-12-19 23:15:12 -0600

    trying out new alias

--- utils/email.js
+++ utils/email.js
@@ -1,6 +1,6 @@
 import nodemailer from 'nodemailer';
 
-const { MAIL_SERVICE, MAIL_USER, MAIL_PASS } = process.env;
+const { MAIL_SERVICE, MAIL_USER } = process.env;
 
 /**
  * Creates transporter object that will help us to send emails
@@ -8,8 +8,7 @@ const { MAIL_SERVICE, MAIL_USER, MAIL_PASS } = process.env;
 const transporter = nodemailer.createTransport({
   service: MAIL_SERVICE,
   auth: {
-    user: MAIL_USER,
-    pass: MAIL_PASS,
+    user: MAIL_USER
   },
 });
 
