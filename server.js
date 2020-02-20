1f3e086050a35e1286ecfa69dbe7d5957342f267
Andrew Reese <ask@hyliancoder.com>
2020-02-19 19:26:12 -0600

    removed graphql headers

--- server.js
+++ server.js
@@ -44,23 +44,6 @@ app.get('/', function(req, res) {
 	res.redirect('/graphql');
 });
 
-app.get('/graphql', function (req, res) {
-  res.setHeader('Content-Type', 'text/event-stream')
-  res.setHeader('Cache-Control', 'no-cache')
- 
-  // send a ping approx every 2 seconds
-  var timer = setInterval(function () {
-    res.write('data: ping\n\n')
- 
-    // !!! this is the important part
-    res.flush()
-  }, 2000)
- 
-  res.on('close', function () {
-    clearInterval(timer)
-  })
-})
-
 app.use(bugsnagClient.getPlugin('express').errorHandler);
 
 // Create a Apollo Server
